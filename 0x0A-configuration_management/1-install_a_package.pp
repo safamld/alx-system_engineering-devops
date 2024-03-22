36aa89f5e7dd7ee64ed92ebdb3589e7a34592e71
safa <safaaamd7@gmail.com>
2024-03-22 05:43:48 -0700

    install package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,10 @@
 #!/usr/bin/pup
 #install flask from pip3
-package { 'flask':
- ensure => '2.1.0',
- provider => 'pip3',
+package { 'python3-pip':
+  ensure => installed,
+}
+exec {'install_flask':
+  command     => '/usr/bin/pip3 install -q Flask==2.1.0',
+  path        => ['/usr/bin'],
+  unless      => '/usr/bin/pip3 show Flask | grep -q "^Version: 2.1.0$"',
 }
