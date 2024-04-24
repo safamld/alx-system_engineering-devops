80a0860690f739c5f2e9bd44ee94fe69707946bc
safamld <safaaamd7@gmail.com>
2024-04-24 07:27:51 -0700

    API

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-#Using what you did in the task #0, extend your Python script to export data in the CSV format.
+"""Using what you did in the task #0, extend your Python script to export data in the CSV format."""
+
+
 import csv
 import requests
 import sys
