4ebc9c98baa3818cbe14b91011a055efcbfe42eb
safamld <safaaamd7@gmail.com>
2024-04-24 08:44:16 -0700

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,12 +3,29 @@
 import requests
 import sys
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+def get_employee_todo_list_progress(employee_id):
+    url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
+    response = requests.get(url)
+    data = response.json()
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+    if not data:
+        print(f'No data found for employee with ID {employee_id}.')
+        return
+
+    employee_name = data[0]['username']
+    done_tasks = [task for task in data if task['completed']]
+    total_tasks = len(data)
+    num_done_tasks = len(done_tasks)
+
+    print(f'Employee {employee_name} is done with tasks ({num_done_tasks}/{total_tasks}):')
+
+    for task in done_tasks:
+        print(f'\t{task["title"]}')
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Usage: python script.py <employee_id>')
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    get_employee_todo_list_progress(employee_id)
