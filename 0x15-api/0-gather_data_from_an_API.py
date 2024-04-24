f5a5cb97a58d8f8daf5c089ab0f0f00fba493a01
safamld <safaaamd7@gmail.com>
2024-04-24 06:58:42 -0700

    API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
-# script must display on the standard output the employee TODO list progress in this exact format
-import json
-import urllib.request
-url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
-try:
-    with urllib.request.urlopen(url) as response:
-        data = json.loads(response.read().decode())
-        
-        completed_tasks = [todo for todo in data if todo['completed']]
-        num_completed_tasks = len(completed_tasks)
-        total_tasks = len(data)
-        
-        employee_name = data[0]['name'] if data else "Unknown"
-        print(f"Employee {employee_name} is done with tasks ({num_completed_tasks}/{total_tasks}):")
-        for task in completed_tasks:
-            print(f"\t{task['title']}")
-except urllib.error.HTTPError as e:
-    print("Failed to fetch TODO list.")
-    print(e)
+"""Returns to-do list information for a given employee ID."""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
